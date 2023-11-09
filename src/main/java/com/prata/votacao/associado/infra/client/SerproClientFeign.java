088f9682989a804a1369603b2e6fd738cd08efc3
matheusprata <matheusprata.ti@gmail.com>
2023-11-08 21:43:08 -0300

    retirada de anotações

--- src/main/java/com/prata/votacao/associado/infra/client/SerproClientFeign.java
+++ src/main/java/com/prata/votacao/associado/infra/client/SerproClientFeign.java
@@ -1,16 +1,10 @@
 package com.prata.votacao.associado.infra.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@Component
-@Service
-@Repository
 @FeignClient(name = "serproClientFeign", url = "https://gateway.apiserpro.serpro.gov.br/consulta-cpf-df-trial")
 public interface SerproClientFeign {
     @GetMapping(value = "/v1/cpf/{cpfAssociado}")

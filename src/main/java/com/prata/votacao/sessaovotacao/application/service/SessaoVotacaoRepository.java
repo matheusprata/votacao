b18c47ab528b80914ae97c0b008a6fb720ff529e
matheusprata <103780607+matheusprata@users.noreply.github.com>
2023-11-29 15:51:12 -0300

    implementação para quando estiver com a sessão fechada após a votação conseguir emitir a mensageria

--- src/main/java/com/prata/votacao/sessaovotacao/application/service/SessaoVotacaoRepository.java
+++ src/main/java/com/prata/votacao/sessaovotacao/application/service/SessaoVotacaoRepository.java
@@ -2,9 +2,11 @@ package com.prata.votacao.sessaovotacao.application.service;
 
 import com.prata.votacao.sessaovotacao.domain.SessaoVotacao;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SessaoVotacaoRepository {
     SessaoVotacao salva(SessaoVotacao sessaoVotacao);
     SessaoVotacao buscaPorId(UUID idSessao);
+    List<SessaoVotacao> buscaAbertas();
 }
--- src/main/java/com/prata/votacao/sessaovotacao/infra/SessaoVotacaoSpringDataJPARepository.java
+++ src/main/java/com/prata/votacao/sessaovotacao/infra/SessaoVotacaoSpringDataJPARepository.java
@@ -1,8 +1,12 @@
 package com.prata.votacao.sessaovotacao.infra;
 
 import com.prata.votacao.sessaovotacao.domain.SessaoVotacao;
+import com.prata.votacao.sessaovotacao.domain.StatusSessaoVotacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.UUID;
 
 public interface SessaoVotacaoSpringDataJPARepository extends JpaRepository<SessaoVotacao, UUID> {
+    List<SessaoVotacao> findByStatus(StatusSessaoVotacao statusSessaoVotacao);
 }
